201a7c52beb224afe2682299b1c25f19b73ed11e
danielbeilin <dbeilin3@gmail.com>
2021-11-29 15:21:28 +0200

    added logic of add new student

--- app/src/main/java/com/example/studentsapp/AddNewStudentActivity.java
+++ app/src/main/java/com/example/studentsapp/AddNewStudentActivity.java
@@ -1,5 +1,6 @@
 package com.example.studentsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,11 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.studentsapp.model.Model;
+import com.example.studentsapp.model.Student;
+
+import java.util.List;
 
 public class AddNewStudentActivity extends AppCompatActivity  {
     EditText nameEt;
@@ -31,6 +37,18 @@ public class AddNewStudentActivity extends AppCompatActivity  {
                 save();
             }
         });
+
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("EDIT","Setting cancel button");
+                Intent sendIntent = new Intent(AddNewStudentActivity.this, StudentListRvAcivity.class);
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                // Start the activity
+                startActivity(sendIntent);
+            }
+        });
     }
 
     private void save() {
@@ -38,5 +56,14 @@ public class AddNewStudentActivity extends AppCompatActivity  {
         String id = idEt.getText().toString();
         boolean flag = cb.isChecked();
         Log.d("TAG","saved name:" + name + " id:" + id + " flag:" + flag);
+        List<Student> data = Model.instance.getAllStudents();
+
+        data.add(new Student(name, id, flag));
+
+        Intent sendIntent = new Intent(AddNewStudentActivity.this, StudentListRvAcivity.class);
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.setType("text/plain");
+        // Start the activity
+        startActivity(sendIntent);
     }
 }
